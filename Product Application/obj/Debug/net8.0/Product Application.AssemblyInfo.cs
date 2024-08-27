ab6f90b30960a9e57b6b2ef13922dab9591c635c
Teodor Coste <teodor.coste22@gmail.com>
2024-08-27 10:30:23 +0300

    commit

--- Product Application/obj/Debug/net8.0/Product Application.AssemblyInfo.cs
+++ Product Application/obj/Debug/net8.0/Product Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b89a97db34841d9bb9e3e564c1cd1f6434c6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c28a63d9277a52500fc408cac7aac9beb73d34a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
